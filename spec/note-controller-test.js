9a2c9b0c6a982c8d55e65a72a1afcdb9aa7a86f3
Luke Cartledge <luke@lukecartledge.com>
2016-08-17 19:52:06 +0100

    Ch9 - completes and tests single note view

--- spec/note-controller-test.js
+++ spec/note-controller-test.js
@@ -14,4 +14,45 @@ function testNoteController() {
   assert.isTrue(noteController._getAppDiv().innerHTML === newView.turnInHtml());
 }
 
+function testDisplaySingleNoteOnClick() {
+  console.log("Test Display Single Note on Click");
+  var note;
+  var noteList;
+  var noteController;
+  var view;
+
+  list = new NoteList();
+  list.createNewNote('This is just a test');
+  view = new ListView(list);
+
+  note = new SingleNoteView(list.getAllNotes()[0]);
+
+  var appDiv = document.createElement('div', {id: 'app'});
+  document.getElementById = function() {
+    return appDiv;
+  };
+
+  noteController = new NoteController(view);
+  noteController.updateHTML();
+  function showNoteForCurrentPage(){
+    noteController.showNote(noteController.getNoteFromUrl(window.location));
+  }
+  window.addEventListener("hashchange", showNoteForCurrentPage);
+
+  appDiv.getElementsByTagName('a')[0].click();
+
+  window.setTimeout(function(){
+    var htmlOutput = "<ul><li><a href=\"#" + list.getAllNotes()[0].noteID + "\">" + note.singleNoteHtml() + "</a></li></ul>";
+    var element = appDiv;
+
+    console.log("expected: ",  htmlOutput);
+    console.log("got: ", element.innerHTML);
+
+    assert.isTrue(htmlOutput === element.innerHTML);
+    // document.getElementById = document.__proto__.getElementById;
+  }, 500);
+
+}
+
 testNoteController();
+testDisplaySingleNoteOnClick();
--- src/controllers/note-controller.js
+++ src/controllers/note-controller.js
@@ -12,5 +12,14 @@
     this._getAppDiv().innerHTML = this._view.turnInHtml();
   };
 
+  NoteController.prototype.getNoteFromUrl = function(location){
+    return location.hash.split('#')[1];
+  };
+
+  NoteController.prototype.showNote = function(id){
+    var singleView = new SingleNoteView(this._view.listOfNotes.getNoteById(id));
+    document.getElementById('app').innerHTML = singleView.getHTML();
+  };
+
   exports.NoteController = NoteController;
 })(this);
